354f79c56fe38906c128bcac4343f5727309db88
M Yusuf Mukharom <42793951+051mym@users.noreply.github.com>
2020-11-29 17:36:14 +0700

    radio btn

--- app/src/main/java/com/example/nanta/kontakmap/UpdateKontakActivity.java
+++ app/src/main/java/com/example/nanta/kontakmap/UpdateKontakActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,13 +26,17 @@ public class UpdateKontakActivity extends AppCompatActivity {
         editNomor = (EditText) findViewById(R.id.editTextNomor);
         editNama = (EditText) findViewById(R.id.editTextNama);
         editNRP = (EditText) findViewById(R.id.editTextTextNRP);
-        editJenisKelamin = (EditText) findViewById(R.id.editTextJenisKelamin);
+//        editJenisKelamin = (EditText) findViewById(R.id.editTextJenisKelamin);
         editAlamat = (EditText) findViewById(R.id.editTextTextAlamat);
         editLatitude = (EditText) findViewById(R.id.updateLatitude);
         editLongitude = (EditText) findViewById(R.id.updateLongitude);
+        final RadioGroup rbg=(RadioGroup) findViewById(R.id.radioGroup1);
+
 
         bSimpan = (Button) findViewById(R.id.buttonSimpan);
         bKembali = (Button) findViewById(R.id.buttonKembali);
+
+
 
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         cursor = db.rawQuery("SELECT * FROM biodata WHERE nama = '" + getIntent().getStringExtra("nama") + "'", null);
@@ -40,7 +46,11 @@ public class UpdateKontakActivity extends AppCompatActivity {
             editNomor.setText(cursor.getString(0).toString());
             editNama.setText(cursor.getString(1).toString());
             editNRP.setText(cursor.getString(2).toString());
-            editJenisKelamin.setText(cursor.getString(3).toString());
+//            editJenisKelamin.setText(cursor.getString(3).toString());
+//            Toast.makeText(getBaseContext(), cursor.getString(3).toString(), Toast.LENGTH_LONG).show();
+            if (cursor.getString(3).toString().equals("Perempuan") ) {
+                rbg.check(R.id.radioperempuan);
+            } else  rbg.check(R.id.radiolaki);
             editAlamat.setText(cursor.getString(4).toString());
             editLatitude.setText(cursor.getString(5).toString());
             editLongitude.setText(cursor.getString(6).toString());
@@ -48,11 +58,13 @@ public class UpdateKontakActivity extends AppCompatActivity {
         bSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int selected = rbg.getCheckedRadioButtonId();
+                RadioButton gender=(RadioButton) findViewById(selected);
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
                 db.execSQL("UPDATE biodata " +
                         "set nama='" + editNama.getText().toString() +
                         "', nrp='" + editNRP.getText().toString() +
-                        "', jenis_kelamin='" + editJenisKelamin.getText().toString() +
+                        "', jenis_kelamin='" + gender.getText().toString() +
                         "', latitude='" + editLatitude.getText().toString() +
                         "', longitude='" + editLongitude.getText().toString() +
                         "'WHERE nomor='" + editNomor.getText().toString() + "'");
